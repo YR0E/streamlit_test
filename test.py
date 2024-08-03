e02d7a03c446c693e194d7d96e0d2c3f5b4e9962
YR0E <87166091+YR0E@users.noreply.github.com>
2024-08-03 18:05:02 +0500

    using cache

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import streamlit as st
 
-df_raw = pd.read_csv('data.csv')
-df_raw = df_raw.rename(columns={'prod_1': 'January', 'prod_4': 'April', 'prod_7': 'July', 'prod_10': 'October'})
-df_raw['water_depth'] = df_raw['water_depth']*100
+@st.cache_data
+def read_file():
+    df = pd.read_csv('data.csv')
+    df = df.rename(columns={'prod_1': 'January', 'prod_4': 'April', 'prod_7': 'July', 'prod_10': 'October'})
+    df['water_depth'] = df['water_depth']*100
+    return df
 
 st.title('Welcome to test!')
 
+df_raw = read_file()
 city_list = df_raw.city.unique()
 city = st.selectbox('Choose a city:', city_list)
 
